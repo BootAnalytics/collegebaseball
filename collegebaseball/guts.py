c795aabb939137d83a859dd436a15c45dd75c09f
BootAnalytics <125611355+BootAnalytics@users.noreply.github.com>
2024-03-04 12:40:48 -0600

    trying to fix parquet

--- collegebaseball/guts.py
+++ collegebaseball/guts.py
@@ -48,7 +48,7 @@ def get_seasons_path():
     """
     """
     with resources.path("collegebaseball.data",
-                        "seasons.csv") as f:
+                        "seasons.parquet") as f:
         data_file_path = f
     return data_file_path
 
